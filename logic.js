ae0a7f200ae629a72a7014c14fe507b5217f387d
changrita1114 <hardheard101@gmail.com>
2021-01-10 00:19:20 -0800

    upload figures

--- logic.js
+++ logic.js
@@ -338,14 +338,14 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/streets-v11",
-  accessToken: API_KEY
+  accessToken: "pk.eyJ1IjoiZG9tZ29uemFsZXMiLCJhIjoiY2tpam96djQwMDIzYjJwcTlibW4zeGYxaCJ9.8LKLu5tJwBBACavlTv1llA"
 });
 
 var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
   id: "dark-v10",
-  accessToken: API_KEY
+  accessToken: "pk.eyJ1IjoiZG9tZ29uemFsZXMiLCJhIjoiY2tpam96djQwMDIzYjJwcTlibW4zeGYxaCJ9.8LKLu5tJwBBACavlTv1llA"
 });
 
 var EastC_Attendance = L.layerGroup(ECAttendance);
