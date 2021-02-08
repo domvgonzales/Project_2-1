0112e417a7c69ceff88af65abcc3872b32bb53f4
Dominic Gonzales <69130409+domvgonzales@users.noreply.github.com>
2021-02-07 20:38:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,67 @@
+from flask import Flask, render_template, jsonify
+import pandas as pandas
+import csv 
+import psycopg2 as psycopg2
 
-from flask import Flask, render_template
+app = Flask(__name__)
 
-app = Flask("app")
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
 
+# @app.route("/spotify_data")
+# def read_spotify_hits_sql():
+#     conn = psycopg.connect(dbname="spotify_data", user="postgres", password="BUChoy123!!!", host="127.0.0.1", port="5432")
+#     dataframe = pd.read_sql("SELECT * FROM spotify_data", conn)
+#     json_data = dataframe.to_json(orient="records")
+#     return json_data
+
+# if __name__ == "main":
+#     app.run(debug=True)
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
 
-@app.route("/attendance")
-def leafletMap():
-    return render_template("leaflet.html")
-
-
-@app.route('/bestGoalkeeper')
-def bar_1():
-    return render_template('bar_1.html')
-
-
-@app.route('/topScorers')
-def bar_2():
-    return render_template('bar_2.html')
-
-
-@app.route('/teamComparisons')
-def bar_3():
-    return render_template('bar_3.html')
-
-
-@app.route('/easternConference')
-def eastern():
-    return render_template('eastern.html')
-
-
-@app.route('/westernConference')
-def western():
-    return render_template('western.html')
-
-
-@app.route('/bracketPredictions')
-def bracker():
-    return render_template('bracket.html')
+@app.route("/hitwords")
+def hitwords():
+    return render_template("hitwords.html")
 
 
 @app.route('/contributors')
-def contributor():
-    return render_template('contributor.html')
+def contributer():
+    return render_template('contributer.html')
+
+
+@app.route('/barcharts')
+def bar_2():
+    return render_template('bars.html')
+
+
+# @app.route('/teamComparisons')
+# def bar_3():
+#     return render_template('bar_3.html')
+
+
+# @app.route('/easternConference')
+# def eastern():
+#     return render_template('eastern.html')
+
+
+# @app.route('/westernConference')
+# def western():
+#     return render_template('western.html')
+
+
+# @app.route('/bracketPredictions')
+# def bracker():
+#     return render_template('bracket.html')
+
+
+# @app.route('/contributors')
+# def contributor():
+#     return render_template('contributor.html')
 
 
 if __name__ == '__main__':
